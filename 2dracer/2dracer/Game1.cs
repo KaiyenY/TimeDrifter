fb886ab32d7c56f1c5f0c371fedcbdab52de7379
mrs5980 <mrs5980@g.rit.edu>
2018-02-21 09:51:26 -0500

    Testing merging conflicts.

--- 2dracer/2dracer/Game1.cs
+++ 2dracer/2dracer/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace _2dracer
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// Matthew is the best architect.
     /// </summary>
     public class Game1 : Game
     {
