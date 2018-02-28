6fd4657d66fe87c9712fef984b90429cb44fd88c
Niko <Niko@MSI>
2018-02-28 10:27:32 -0500

    Removed annoying comments Started input and turret code I wrote plenty of comments

--- 2dracer/2dracer/Game1.cs
+++ 2dracer/2dracer/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace _2dracer
 {
@@ -22,66 +23,70 @@ namespace _2dracer
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            // show the mouse
+            this.IsMouseVisible = true;
             base.Initialize();
         }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             comicSans = Content.Load<SpriteFont>("comic");
-            // TODO: use this.Content to load your game content here
         }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+        
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        // variables for the turret
+        static double x = 0;
+        static double y = 0;
+        static double angle = 0;
+        static bool shooting;
+
+        static void CalculateTurretData(float centerX, float centerY)
+        {
+            // Mouse position is compared to the center of the screen
+            // In the future, we will compare it to the position of the car
+
+            MouseState s = Mouse.GetState();
+            shooting = (s.LeftButton == ButtonState.Pressed);
+
+            x = s.Position.X - centerX;
+            y = s.Position.Y - centerY;
+            angle = Math.Atan(y / x) * 180/3.14159;
+
+            if (x < 0)
+            {
+                angle += 180;
+            }
+
+            if (angle < 0)
+                angle += 360;
+        }
+
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Get data, comparing mouse position to center of screen
+            CalculateTurretData(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            string text = "Checking distance between mouse\nand center of screen\nfor gun on roof: \nX: " + x + "\nY: " + y + "\nangle: " + angle;
+
             spriteBatch.Begin();
-            spriteBatch.DrawString(comicSans, "Hello World\nAnd Goodbye!", new Vector2(GraphicsDevice.Viewport.Width/2, GraphicsDevice.Viewport.Height/2), Color.White);
+            spriteBatch.DrawString(comicSans, text, new Vector2(GraphicsDevice.Viewport.Width/2, GraphicsDevice.Viewport.Height/2), Color.White);
             spriteBatch.End();
 
             base.Draw(gameTime);
