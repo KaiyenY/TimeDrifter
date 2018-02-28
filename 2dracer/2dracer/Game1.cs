9d656218a5b7d775180fcd8b5a58115064bbedb5
Niko <Niko@MSI>
2018-02-28 11:04:08 -0500

    Put all Turret code into its own class

--- 2dracer/2dracer/Game1.cs
+++ 2dracer/2dracer/Game1.cs
@@ -5,10 +5,6 @@ using System;
 
 namespace _2dracer
 {
-    /// <summary>
-    //ruben was here
-    /// Matthew is the best architect.
-    /// </summary>
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
@@ -16,9 +12,7 @@ namespace _2dracer
 
         //spritefont
         SpriteFont comicSans;
-
-        //arrow
-        Texture2D arrow;
+        Turret turret1;
 
         public Game1()
         {
@@ -38,47 +32,26 @@ namespace _2dracer
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             comicSans = Content.Load<SpriteFont>("comic");
-            arrow = Content.Load<Texture2D>("arrow");
+
+            Texture2D arrow = Content.Load<Texture2D>("arrow");
+            turret1 = new Turret(arrow);
         }
         
         protected override void UnloadContent()
         {
         }
-
-        // variables for the turret
-        static double x = 0;
-        static double y = 0;
-        static double angle = 0;
-        static bool shooting;
-
-        static void CalculateTurretData(float centerX, float centerY)
-        {
-            // Mouse position is compared to the center of the screen
-            // In the future, we will compare it to the position of the car
-
-            MouseState s = Mouse.GetState();
-            shooting = (s.LeftButton == ButtonState.Pressed);
-
-            x = s.Position.X - centerX;
-            y = s.Position.Y - centerY;
-            angle = Math.Atan(y / x) * 180/3.14159;
-
-            if (x < 0)
-            {
-                angle += 180;
-            }
-
-            if (angle < 0)
-                angle += 360;
-        }
-
+        
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // Get data, comparing mouse position to center of screen
-            CalculateTurretData(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            // update turret position to car position
+            // or in this case, the center of the screen
+            turret1.setPosition(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+
+            // get angle
+            turret1.calcAngle();
 
             base.Update(gameTime);
         }
@@ -87,22 +60,12 @@ namespace _2dracer
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            string text = "Checking distance between mouse\nand center of screen\nfor gun on roof: \nX: " + x + "\nY: " + y + "\nangle: " + angle;
+            string text = turret1.Debug();
 
             spriteBatch.Begin();
-
-
-
-
-            spriteBatch.Draw(arrow, 
-                new Rectangle(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2, arrow.Width, arrow.Height), null, Color.White, (float)((angle+90) * 3.14159 / 180), new Vector2(arrow.Width / 2, arrow.Height / 2), SpriteEffects.None, 0f);//draw it :D
-
-
-
+            turret1.Draw(spriteBatch);
 
             spriteBatch.DrawString(comicSans, text, new Vector2(GraphicsDevice.Viewport.Width/2, GraphicsDevice.Viewport.Height/2), Color.White);
-
-
             spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ 2dracer/2dracer/Turret.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _2dracer
+{
+    class Turret
+    {
+        Texture2D t;
+
+        double posX;
+        double posY;
+
+        public Turret(Texture2D tex)
+        {
+            t = tex;
+        }
+
+        public void setPosition(double x, double y)
+        {
+            posX = x;
+            posY = y;
+        }
+        
+        double dirX = 0;
+        double dirY = 0;
+        double angle = 0;
+        bool shooting;
+
+        public void calcAngle()
+        {
+            // Mouse position is compared to the center of the screen
+            // In the future, we will compare it to the position of the car
+
+            MouseState s = Mouse.GetState();
+            shooting = (s.LeftButton == ButtonState.Pressed);
+
+            dirX = s.Position.X - posX;
+            dirY = s.Position.Y - posY;
+            angle = Math.Atan(dirY / dirX) * 180 / 3.14159;
+
+            if (dirX < 0)
+            {
+                angle += 180;
+            }
+
+            if (angle < 0)
+                angle += 360;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(t,
+                new Rectangle((int)posX, (int)posY, t.Width, t.Height), 
+                null, 
+                Color.White, 
+                (float)((angle + 90) * 3.14159 / 180), 
+                new Vector2(t.Width / 2, t.Height / 2), 
+                SpriteEffects.None, 0f);
+        }
+
+        public String Debug()
+        {
+            String text = "Turret Debug:" +
+                "\nangle: " + angle + "\nShooting (left click): ";
+            
+            if (shooting)
+                text += "Yes";
+            else
+                text += "No";
+
+            return text;
+        }
+    }
+}
