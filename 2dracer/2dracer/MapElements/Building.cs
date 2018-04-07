ad2adf1db3cdf54aa03a4db8ddc600dea5b1a62a
Niko <Niko@MSI>
2018-04-07 15:33:28 -0400

    Comments

--- 2dracer/2dracer/MapElements/Building.cs
+++ 2dracer/2dracer/MapElements/Building.cs
@@ -24,7 +24,21 @@ namespace _2dracer.MapElements
         public void Draw()
         {
             BasicEffect effect = (BasicEffect)model.Meshes[0].Effects[0];
-            
+
+            // Get/Set for texture
+            //effect.Texture
+
+            // the list of textures on the cube will either be
+            // model.Meshes[i].Effects[0].Texture
+            // or
+            // model.Meshes[0].Effects[i].Texture
+            //
+            // We will figure out later
+
+            // Right now there is only model.Meshes[0].Effects[0]
+            // To add more textures, I need to make adjustments to the 3D model
+            // This will be done later
+
             float aspectRatio = Game1.graphics.PreferredBackBufferWidth / (float)Game1.graphics.PreferredBackBufferHeight;
             float fieldOfView = MathHelper.PiOver4;
 
