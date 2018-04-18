d4e8711365d0ed6818f729edead683df6f2abbe1
njp2424 <njp2424@IGMOH1380-22>
2018-04-18 09:02:46 -0400

    Size of buildings is the same, regardless of screen size No Depth Buffer

--- 2dracer/2dracer/MapElements/Building.cs
+++ 2dracer/2dracer/MapElements/Building.cs
@@ -25,7 +25,7 @@ namespace _2dracer.MapElements
             BasicEffect effect = (BasicEffect)model.Meshes[0].Effects[0];
             effect.Texture = img;
             
-            float fieldOfView = MathHelper.PiOver4;
+            double fieldOfView = (3.14159 / 4) * Options.Graphics.GraphicsDevice.Viewport.Width/1500;
 
             // This transfers 2d coordinates to 3d world space coordinates
             Vector3 modelPos = Options.Graphics.GraphicsDevice.Viewport.Unproject(
@@ -36,16 +36,7 @@ namespace _2dracer.MapElements
 
             int z = 5;
 
-            effect.Projection = Matrix.CreatePerspectiveFieldOfView(fieldOfView, Options.Graphics.GraphicsDevice.Viewport.AspectRatio, 0.1f, 200f);
-            // effect.View = Matrix.CreateTranslation(-1 * Game1.camera.Position.X / (58 * z), Game1.camera.Position.Y / (58 * z), (float)-10 / z);
-            // effect.World = Matrix.CreateTranslation(Position.X, Position.Y, 0f);
-
-            // I worked out some code to make it look like the above, except we don't need
-            // to hard code any numbers to get world coordinates. Not sure what the 2.3f is
-            // but I'm thinking it's the height of the model. When it's set to 0 the model is
-            // rendered with the 2d plane on the roofs. If you want to change the code back
-            // just comment out these lines, uncomment the ones above, and changes the comments
-            // in map where buildings are created.
+            effect.Projection = Matrix.CreatePerspectiveFieldOfView((float)fieldOfView, Options.Graphics.GraphicsDevice.Viewport.AspectRatio, 0.1f, 200f);
             effect.View = Matrix.CreateLookAt(new Vector3(Vector2.Zero, 2.3f), Vector3.Zero, Vector3.Up);
             effect.World = Matrix.CreateTranslation(modelPos.X, modelPos.Y, 0f);
 
