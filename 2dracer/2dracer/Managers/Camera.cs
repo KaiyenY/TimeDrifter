1ffb1ad1763a55ad9a8e4c941306161310bd0ef5
Niko <Niko@MSI>
2018-04-20 14:55:24 -0400

    Fixed A Bug

--- 2dracer/2dracer/Managers/Camera.cs
+++ 2dracer/2dracer/Managers/Camera.cs
@@ -38,7 +38,19 @@ namespace _2dracer.Managers
             {
                 position.Y = pos.Y - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5;
             }
-            
+
+            if (position.X < (Options.ScreenWidth / 2) - 384 - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5)
+                position.X = (Options.ScreenWidth / 2) - 384 - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5;
+
+            if (position.X > (Options.ScreenWidth / 2) + mapSize.X - 384 - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5)
+                position.X = (Options.ScreenWidth / 2) + mapSize.X - 384 - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5;
+
+            if (position.Y < (Options.ScreenHeight / 2) - 384 - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5)
+                position.Y = (Options.ScreenHeight / 2) - 384 - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5;
+
+            if (position.Y > (Options.ScreenHeight / 2) + mapSize.Y - 384 - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5)
+                position.Y = (Options.ScreenHeight / 2) + mapSize.Y - 384 - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5;
+
             ViewMatrix = Matrix.CreateTranslation(new Vector3(-position, 0));
         }
     }
