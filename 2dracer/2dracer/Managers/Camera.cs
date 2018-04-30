d7855f09c7111ba554a000d46015f923d48b3214
Niko <Niko@MSI>
2018-04-29 23:16:36 -0400

    Camera Interpolation and Intro Cutscene

--- 2dracer/2dracer/Managers/Camera.cs
+++ 2dracer/2dracer/Managers/Camera.cs
@@ -30,30 +30,51 @@ namespace _2dracer.Managers
             float tileSize = Map.TileSize;
             ViewRect = new Rectangle(Position.ToPoint(), new Point(Options.ScreenWidth, Options.ScreenHeight));
 
-            // Through complicated stuff, these two checks will make sure the
-            // camera doesn't go off the map
-            if (pos.X >= (Options.ScreenWidth / 2) - (tileSize / 2) &&
-                pos.X <= -(Options.ScreenWidth / 2) + mapSize.X - (tileSize / 2))
+
+            if (Math.Abs(Player.playerVelocity.X) > 50 || Math.Abs(Player.playerVelocity.Y) > 50)
             {
-                position.X = pos.X - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5;
+                // Through complicated stuff, these two checks will make sure the
+                // camera doesn't go off the map
+                if (pos.X >= (Options.ScreenWidth / 2) - (tileSize / 2) &&
+                    pos.X <= -(Options.ScreenWidth / 2) + mapSize.X - (tileSize / 2))
+                {
+                    position.X = pos.X - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5;
+                }
+                if (pos.Y >= (Options.ScreenHeight / 2) - (tileSize / 2) &&
+                    pos.Y <= -(Options.ScreenHeight / 2) + mapSize.Y - (tileSize / 2))
+                {
+                    position.Y = pos.Y - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5;
+                }
+
+                if (position.X < -1* (tileSize / 2) + Player.playerVelocity.X / 5)
+                    position.X = -1* (tileSize / 2) + Player.playerVelocity.X / 5;
+
+                if (position.X > mapSize.X - (tileSize / 2) + Player.playerVelocity.X / 5)
+                    position.X = mapSize.X - (tileSize / 2) + Player.playerVelocity.X / 5;
+
+                if (position.Y <  -1 * (tileSize / 2) + Player.playerVelocity.Y / 5)
+                    position.Y =  -1 * (tileSize / 2) + Player.playerVelocity.Y / 5;
+
+                if (position.Y >  mapSize.Y - (tileSize / 2) + Player.playerVelocity.Y / 5)
+                    position.Y =  mapSize.Y - (tileSize / 2) + Player.playerVelocity.Y / 5;
             }
-            if (pos.Y >= (Options.ScreenHeight / 2) - (tileSize / 2) &&
-                pos.Y <= -(Options.ScreenHeight / 2) + mapSize.Y - (tileSize / 2))
+
+            else
             {
-                position.Y = pos.Y - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5;
+                int speed = 10;
+
+                if (pos.X > position.X + (Options.ScreenWidth / 2))
+                    position.X += speed;
+
+                if (pos.Y > position.Y + (Options.ScreenHeight / 2))
+                    position.Y += speed;
+
+                if (pos.X < position.X + (Options.ScreenWidth / 2))
+                    position.X -= speed;
+
+                if (pos.Y < position.Y + (Options.ScreenHeight / 2))
+                    position.Y -= speed;
             }
-            
-            if (position.X < (Options.ScreenWidth / 2) - (tileSize / 2) - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5)
-                position.X = (Options.ScreenWidth / 2) - (tileSize / 2) - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5;
-
-            if (position.X > (Options.ScreenWidth / 2) + mapSize.X - (tileSize / 2) - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5)
-                position.X = (Options.ScreenWidth / 2) + mapSize.X - (tileSize / 2) - (Options.ScreenWidth / 2) + Player.playerVelocity.X / 5;
-
-            if (position.Y < (Options.ScreenHeight / 2) - (tileSize / 2) - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5)
-                position.Y = (Options.ScreenHeight / 2) - (tileSize / 2) - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5;
-
-            if (position.Y > (Options.ScreenHeight / 2) + mapSize.Y - (tileSize / 2) - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5)
-                position.Y = (Options.ScreenHeight / 2) + mapSize.Y - (tileSize / 2) - (Options.ScreenHeight / 2) + Player.playerVelocity.Y / 5;
 
             ViewMatrix = Matrix.CreateTranslation(new Vector3(-position, 0));
         }
